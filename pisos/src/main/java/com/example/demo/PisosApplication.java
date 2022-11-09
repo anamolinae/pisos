571891fad42608725075961f861369e52d2de7a3
anamolinae <anamari_me@hotmail.com>
2022-11-09 18:24:16 +0100

    first commit

---
+++ pisos/src/main/java/com/example/demo/PisosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PisosApplication.class, args);
+	}
+
+}
